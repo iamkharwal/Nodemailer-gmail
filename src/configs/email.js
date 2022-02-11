152b318c8a034e66df0499cee147fe7521456219
Atul <atulkharwal9329@gmail.com>
2022-02-12 02:20:21 +0530

    Update email.js

--- src/configs/email.js
+++ src/configs/email.js
@@ -3,9 +3,7 @@
 const nodemailer = require("nodemailer");
 
 module.exports = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  protocol: ssl,
+  service: "gmail",
   auth: {
     user: "9329hitman@gmail.com",
     pass: "8219947916@Atul",
