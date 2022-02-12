4b926b5508323ebc5d8024b75bb0a94509da0efd
Atul Kharwal <93377463+iamkharwal@users.noreply.github.com>
2022-02-12 15:16:45 +0530

    Update email.js

--- src/configs/email.js
+++ src/configs/email.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 module.exports = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "9329hitman@gmail.com",
-    pass: "8219947916@Atul",
+    user: "xxxxx",
+    pass: "xxxxx",
   },
 });
